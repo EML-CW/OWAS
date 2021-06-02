a9b1583a7d223d9ed65c49d11fb05c3552998522
Nathan <nathan.meneguz@epitech.eu>
2021-06-02 15:43:45 +0200

    [~] - Correcting some warnings in the settings view

--- owas-client/src/components/SettingsView/SettingsView.js
+++ owas-client/src/components/SettingsView/SettingsView.js
@@ -12,12 +12,12 @@ const SettingsView = (props) => {
     const host = process.env.REACT_APP_HOST;
     return (
         <Container>
-        <Segment textAlign="justified" color="grey">
+        <Segment textAlign="center" color="grey">
             OWAS v0.1<br/>
             This is alpha software. It is not ready for business use. Use at your own risk<br/>
             Copyright OWAS 2021
         </Segment>
-        <Segment text color="grey">
+        <Segment color="grey">
             Server host: { host }
         </Segment>
         <Button onClick={logOut} fluid color="red" basic icon>
