2988477bc874c4c3eb8804a142b7e1a26d57d2dc
Nathan <nathan.meneguz@epitech.eu>
2021-06-04 11:33:38 +0200

    [+] - Customer deletion added. You can now call the customer by tapping its phone number

--- owas-client/src/components/App/CustomerCard/CustomerCard.js
+++ owas-client/src/components/App/CustomerCard/CustomerCard.js
@@ -1,8 +1,25 @@
+import axios from "axios";
 import { useState } from "react";
-import { Card, Button, Form } from "semantic-ui-react";
+import { Card, Button, Form, Segment } from "semantic-ui-react";
+import qs from "querystring"
 
 const CustomerCard = (props) => {
+    const [loadingState, setLoadingState] = useState(false);
     const [editState, setEditState] = useState(false)
+    const onClientDelete = () => {
+        setLoadingState(true);
+        const host = process.env.REACT_APP_HOST;
+        axios.post(`http://${host}/clients/deleteclient`, qs.stringify({ clientId: props.id, token: props.token }), { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+            .then((res) => {
+                console.log("yay")
+                setLoadingState(false);
+                props.setLoadCustomers(true);
+            })
+            .catch((err) => {
+                props.setLoadCustomers(true);
+            })
+    }
+
     if (!editState)
         return (
             <Card fluid>
@@ -17,49 +34,51 @@ const CustomerCard = (props) => {
                         Réservations: 0
                     </Card.Description>
                     <Card.Description>
-                        +33 7 00 00 00 00
+                        <a href={`tel:${props.clientPhone}`}>{props.clientPhone}</a>
                     </Card.Description>
                 </Card.Content>
                 <Card.Content extra>
                     <Button basic color="blue" onClick={() => setEditState(true)}>Editer</Button>
-                    <Button basic color="red">Supprimer</Button>
+                    <Button basic color="red" onClick={onClientDelete}>Supprimer</Button>
                 </Card.Content>
             </Card>
         )
     return (
-        <Card fluid>
-            <Card.Content>
-                <Card.Header>
-                    Editer
+        <Segment loading={loadingState}>
+            <Card fluid>
+                <Card.Content>
+                    <Card.Header>
+                        Editer
                 </Card.Header>
-                <Card.Meta>
-                    Editez les champs ci dessous puis validez
+                    <Card.Meta>
+                        Editez les champs ci dessous puis validez
                 </Card.Meta>
-                <Card.Description>
-                    <Form size="large">
-                        <Form.Field>
-                            <input defaultValue={props.clientName}/>
-                        </Form.Field>
-                        <Form.Field>
-                            <input defaultValue={props.clientLastName}/>
-                        </Form.Field>
-                        <Form.Field>
-                            <input defaultValue={props.clientStreet}/>
-                        </Form.Field>
-                        <Form.Field>
-                            <input defaultValue={props.clientZIP}/>
-                        </Form.Field>
-                        <Form.Field>
-                            <input defaultValue={props.clientCity}/>
-                        </Form.Field>
-                    </Form>
-            </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <Button basic color="green" onClick={() => setEditState(true)} >Editer</Button>
-                <Button basic color="red" onClick={() => setEditState(false)}>Annuler</Button>
-            </Card.Content>
-        </Card>
+                    <Card.Description>
+                        <Form size="large">
+                            <Form.Field>
+                                <input defaultValue={props.clientName} />
+                            </Form.Field>
+                            <Form.Field>
+                                <input defaultValue={props.clientLastName} />
+                            </Form.Field>
+                            <Form.Field>
+                                <input defaultValue={props.clientStreet} />
+                            </Form.Field>
+                            <Form.Field>
+                                <input defaultValue={props.clientZIP} />
+                            </Form.Field>
+                            <Form.Field>
+                                <input defaultValue={props.clientCity} />
+                            </Form.Field>
+                        </Form>
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <Button basic color="green" onClick={() => setEditState(true)} >Editer</Button>
+                    <Button basic color="red" onClick={() => setEditState(false)}>Annuler</Button>
+                </Card.Content>
+            </Card>
+        </Segment>
     )
 }
 
--- owas-client/src/components/CustomersView/CustomersView.js
+++ owas-client/src/components/CustomersView/CustomersView.js
@@ -6,8 +6,14 @@ import CustomerCard from "../App/CustomerCard/CustomerCard";
 const CustomersView = (props) => {
     const [customerComponentArray, setCustomerComponentArray] = useState([]);
     const [loadingState, setLoadingState] = useState(false);
+    const [loadCustomers, setLoadCustomers] = useState(true);
 
     useEffect(() => {
+        console.log(loadCustomers);
+        if (loadCustomers === false) {
+            console.log("Not reloading")
+            return;
+        }
         setLoadingState(true);
         let customerArray = [];
         const host = process.env.REACT_APP_HOST;
@@ -16,20 +22,28 @@ const CustomersView = (props) => {
                 console.log(res.data);
                 customerArray = res.data.clientList.map(data => {
                     return (
-                        <CustomerCard clientName={data._clientName}
+                        <CustomerCard
+                            setLoadCustomers={setLoadCustomers}
+                            key={data._id}
+                            id={data._id}
+                            token={props.token}
+                            clientName={data._clientName}
                             clientLastName={data._clientLastName}
                             clientStreet={data._clientStreet}
                             clientZIP={data._clientZIP}
-                            clientCity={data._clientCity} />
+                            clientCity={data._clientCity}
+                            clientPhone={data._clientPhone} />
                     )
                 })
                 setCustomerComponentArray(customerArray);
                 setLoadingState(false);
+                setLoadCustomers(false);
             })
             .catch((err) => {
+                setLoadCustomers(false);
                 setLoadingState(false);
             })
-    }, [setCustomerComponentArray, props.token])
+    }, [setCustomerComponentArray, props.token, setLoadCustomers, loadCustomers])
     return (
         <Segment basic loading={loadingState}>
             <Button basic color="blue" onClick={() => props.setActiveMenu("newcustomer")}>Nouveau client</Button>
