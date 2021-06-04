09f993f974acc5af322555074a8ec12b26b80c7b
Nathan <nathan.meneguz@epitech.eu>
2021-06-04 14:54:04 +0200

    [+] - Creating all necessary routes form reservation handling. Untested

---
+++ owas-server/src/routes/resa.routes.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const Router = express.Router();
+const mongoWrapper = require('../services/mongo.service')
+const bodyParser = require('body-parser');
+const token = require('../middlewares/tokencheck.middleware');
+const { tokenCheck } = require('../middlewares/tokencheck.middleware');
+
+Router.use((req, res, next) => {
+    const date = new Date();
+    console.log(`[🕔] -- ${date.getHours()}:${date.getMinutes()} -> ${req.ip}`);
+    next();
+});
+
+Router.post('/newreservation', token.tokenCheck, (req, res) => {
+    if (!req.body.token || !req.body.clientId || !req.body.bikeId || !req.body.from || !req.body.to) {
+        res.status(400).send({status: 400, message: "Bad request"});
+        return;
+    }
+    mongoWrapper.newEntry('reservations', {
+        _bikeId: req.body.bikeId,
+        _clientId: req.body.clientId,
+        _from: req.body.from,
+        _to: req.body.to
+    }, (success) => {
+        if (success) {
+            res.status(200).send({status: 200, message: "Ok."})
+            return;
+        }
+        res.status(500).send({status: 500, message: "Internal server error"});
+        return;
+    })
+})
+
+Router.post('/deletereservation', token.tokenCheck, (req,res) => {
+    if (!req.body.token || !req.body.reservationId) {
+        res.status(400).send({status: 400, message: "Bad request"});
+        return;
+    }
+    mongoWrapper.deleteOne('reservations', {_id: req.body.reservationId}, (success) => {
+        if (success) {
+            res.status(200).send({status: 200, message: "Ok."});
+            return;
+        }
+        res.status(500).send({status: 500, message: "Internal server error."});
+        return;
+    })
+})
+
+Router.post('updatereservation', token.tokenCheck, (req, res) => {
+    if (!req.body.token || !req.body.reservationId || !req.body.clientId || !req.body.bikeId || !req.body.from || !req.body.to) {
+        res.status(400).send({status: 400, message: "Bad request"});
+    }
+    mongoWrapper.updateById('reservations', req.body.reservationId, {
+        _bikeId: req.body.bikeId,
+        _clientId: req.body.clientId,
+        _from: req.body.from,
+        _to: req.body.to
+    }, (success) => {
+        success ? res.status(200).send({status: 200, message:"Ok."}) : res.status(500).send({status: 500, message: "Internal server error"});
+        return;
+    })
+})
