8dc99f683d858af7ad9bf3f834d4dd8af0911174
Nathan <nathan.meneguz@epitech.eu>
2021-06-06 10:43:45 +0200

    [+] - Reservation edition implemented

--- owas-client/src/components/CalendarView/CalendarView.js
+++ owas-client/src/components/CalendarView/CalendarView.js
@@ -7,11 +7,13 @@ import axios from "axios";
 const CalendarView = (props) =>{
     const [reservations, setReservations] = useState([]);
     const [loadingState, setLoadingState] = useState(false);
+    const [loadReservations, setloadReservations] = useState(true);
     useEffect(() => {
         const host = process.env.REACT_APP_HOST;
         setLoadingState(true);
         let reservationsArray = [];
-
+        if (loadReservations === false)
+            return;
             axios.get(`http://${host}/bikes/fetchbikes?token=${props.token}`)
             .then((resBikes) => {
                 axios.get(`http://${host}/clients/fetchclientlist?token=${props.token}`)
@@ -27,17 +29,20 @@ const CalendarView = (props) =>{
                                 reservationId={data._id}
                                 clientId={data._clientId}
                                 from={data._from}
-                                to={data._to} />)
+                                to={data._to}
+                                setloadReservations={setloadReservations} />)
                         })
                         setReservations(reservationsArray);
                         setLoadingState(false);
+                        setloadReservations(false);
                     })
                     .then(err => {
+                        setloadReservations(false);
                         setLoadingState(false);
                     })
                 })
             })
-    }, [setLoadingState, setReservations, props.token])
+    }, [setLoadingState, setReservations, setloadReservations, loadReservations, props.token])
     return (
         <Segment basic fluid="true" loading={loadingState}>
             <Header as="h1">Calendrier</Header>
--- owas-client/src/components/ReservationCard/ReservationCard.js
+++ owas-client/src/components/ReservationCard/ReservationCard.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Card, Button, Select, Input, Label, Form } from 'semantic-ui-react';
+import qs from 'querystring';
 
 const ReservationCard = (props) => {
     const [bikeNode, setBikeNode] = useState({})
@@ -8,11 +9,11 @@ const ReservationCard = (props) => {
     const [editState, setEditState] = useState(false);
     const [dropDownBikes, setDropDownBikes] = useState([]);
     const [dropDownClients, setDropDownClients] = useState([]);
-    const [formattedDates, setFormattedDates] = useState({
+    const [formattedDates] = useState({
         from: new Date(props.from),
         to: new Date(props.to),
     })
-    console.log(formattedDates);
+
     useEffect(() => {
         let picked = props.bikeList.find(object => object._id === props.bikeId)
         setBikeNode(picked);
@@ -26,6 +27,69 @@ const ReservationCard = (props) => {
         }))
 
     }, [setBikeNode, setClientNode, props.bikeList, props.clientList, props.token, props.bikeId, props.clientId])
+
+    const [editValues, setEditValues] = useState({
+        reservationId: props.reservationId,
+        clientId: clientNode._id,
+        bikeId: bikeNode._id,
+        from: `${formattedDates.from.getFullYear()}-${formattedDates.from.getMonth()}-${formattedDates.from.getDay()}`,
+        to: `${formattedDates.to.getFullYear()}-${formattedDates.to.getMonth()}-${formattedDates.to.getDay()}`,
+        token: props.token
+    })
+
+    const onClientChange = (e, data) => {
+        console.log(data.value);
+        setEditValues({
+            reservationId: editValues.reservationId,
+            clientId: data.value,
+            bikeId: editValues.bikeId,
+            from: editValues.from,
+            to: editValues.to,
+            token: editValues.token
+        })
+    }
+    const onBikeChange = (e, data) => {
+        setEditValues({
+            reservationId: editValues.reservationId,
+            clientId: editValues.clientId,
+            bikeId: data.value,
+            from: editValues.from,
+            to: editValues.to,
+            token: editValues.token
+        })
+    }
+    const onFromChange = (e) => {
+        setEditValues({
+            reservationId: editValues.reservationId,
+            clientId: editValues.clientId,
+            bikeId: editValues.bikeId,
+            from: e.target.value,
+            to: editValues.to,
+            token: editValues.token
+        })
+    }
+    const onToChange = (e) => {
+        setEditValues({
+            reservationId: editValues.reservationId,
+            clientId: editValues.clientId,
+            bikeId: editValues.bikeId,
+            from: editValues.from,
+            to: e.target.value,
+            token: editValues.token
+        })
+    }
+
+    const onEditSubmit = () => {
+        const host = process.env.REACT_APP_HOST
+        console.log(editValues);
+        const formData = qs.stringify(editValues);
+        axios.post(`http://${host}/reservations/updatereservation`, formData, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+        .then((res) => {
+            props.setloadReservations(true);
+            setEditState(false);
+        })
+    }
+    // Rendering card
     if (editState === false)
         return (
             <Card fluid color="blue">
@@ -49,21 +113,21 @@ const ReservationCard = (props) => {
                     <Card.Meta>Remplissez les champs ci-dessous et validez</Card.Meta>
                     <Form>
                             <Form.Field>
-                                <Select fluid defaultValue={bikeNode._id} placeholder="Moto" options={dropDownBikes} />
+                                <Select onChange={onBikeChange} fluid defaultValue={bikeNode._id} placeholder="Moto" options={dropDownBikes} />
                             </Form.Field>
                             <Form.Field>
-                                <Select fluid defaultValue={clientNode._id} placeholder="Client" options={dropDownClients} />
+                                <Select onChange={onClientChange} fluid defaultValue={clientNode._id} placeholder="Client" options={dropDownClients} />
                             </Form.Field>
                             <Form.Field>
-                                <Label pointing="below">Départ:</Label><Input fluid type="date" />
+                                <Label pointing="below">Départ:</Label><Input onChange={onFromChange} fluid type="date" />
                             </Form.Field>
                             <Form.Field>
-                                <Label pointing="below">Arrivée:</Label><Input defaultValue={formattedDates.to}fluid type="date" />
+                                <Label pointing="below">Arrivée:</Label><Input onChange={onToChange} defaultValue={formattedDates.to}fluid type="date" />
                             </Form.Field>
                         </Form>
                     </Card.Content>
                     <Card.Content extra>
-                        <Button color="green" onClick={() => setEditState(false)} basic>Valider</Button>
+                        <Button color="green" onClick={onEditSubmit} basic>Valider</Button>
                         <Button color="red" onClick={() => setEditState(false)} basic>Annuler</Button>
                     </Card.Content>
             </Card>
